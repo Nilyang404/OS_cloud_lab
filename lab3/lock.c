e83a29e5cfe8eba45f82746431d3c056f6facf93
Nilyang404 <63556313+Nilyang404@users.noreply.github.com>
2021-01-06 09:33:23 +0800

    Add files via upload

---
+++ lab3/lock.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+	int	pid, i;
+    printf("\n");
+	if((pid=fork())<0)
+	{       printf("failed to create child \n");
+		return 0;
+	}
+	else if(pid==0)
+	{        lockf(1,1,0); // 锁定标准输出设备(显示器)
+                for(i=0;i<5;i++)
+		     printf("This is child (pid=%d) process:b\n",getpid());
+		lockf(1,0,0); // 解锁标准输出设备(显示器)
+		return 0;
+	}
+    else
+    {  lockf(1,1,0);
+       for(i=0;i<5;i++)
+           printf("Parent process:a\n");
+       lockf(1,0,0);
+       return 0;
+    }
+}
